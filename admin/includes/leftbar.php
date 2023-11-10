8803560c7e0f16db74d8554b079a30c13e3be7de
yanwong127 <rezeyan127@gmail.com>
2023-11-10 09:09:44 +0800

    admin side reupload

---
+++ admin/includes/leftbar.php
@@ -0,0 +1,33 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">Main</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+			
+<li><a href="#"><i class="fa fa-files-o"></i> Item </a>
+<ul>
+<li><a href="add_item.php">Add Item</a></li>
+<li><a href="manage-item.php">Manage Item</a></li>
+</ul>
+</li>
+
+<li><a href="#"><i class="fa fa-area-chart"></i>Place</a>
+					<ul>
+						<li><a href="add_place.php">Add Place</a></li>
+						<li><a href="manage-place.php">Manage Place</a></li>
+					</ul>
+				</li>
+				<li><a href="#"><i class="fa fa-book"></i>Manage Booking</a>
+					<ul>
+						<li><a href="add_place.php">Item Booking</a></li>
+						<li><a href="manage-place.php">Place Booking</a></li>
+					</ul>
+				</li>
+				<li><a href="reg-users.php"><i class="fa fa-users"></i>Manage Users</a></li>
+				<li><a href="testimonials.php"><i class="fa fa-table"></i> Manage Testimonials</a></li>
+				<li><a href="manage-conactusquery.php"><i class="fa fa-desktop"></i> Manage Conatctus Query</a></li>
+
+
+
+			</ul>
+		</nav>
