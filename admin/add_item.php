17f98375c66fc656febbc1695fd082b8ccb95825
yanwong127 <rezeyan127@gmail.com>
2023-11-21 14:53:32 +0800

    img upload

--- admin/add_item.php
+++ admin/add_item.php
@@ -5,32 +5,52 @@ include('includes/config.php');
 if (strlen($_SESSION['alogin']) == 0) {
     header('location:index.php');
 } else {
-
     if (isset($_POST['submit'])) {
         $item_name = $_POST['item_name'];
         $item_overview = $_POST['item_overview'];
-        $item_img = $_FILES["item_img"]["name"];
         $availability = $_POST['availability'];
+        
+        $item_img = $_FILES["item_img"]["name"];
+        $temp_img = $_FILES["item_img"]["tmp_name"];
+        
+        $extension = pathinfo($item_img, PATHINFO_EXTENSION);
+        $destination1 = 'img/image/' . $item_img;  // First folder
+        $destination2 = '../user/img/' . $item_img;  // Second folder
 
-        move_uploaded_file($_FILES["item_img"]["tmp_name"], "img/image/" . $_FILES["item_img"]["name"]);
-
-        $sql = "INSERT INTO item(item_name,item_overview, item_img,availability) VALUES(:item_name, :item_overview, :item_img, :availability)";
-        $query = $dbh->prepare($sql);
-        $query->bindParam(':item_name', $item_name, PDO::PARAM_STR);
-        $query->bindParam(':item_overview', $item_overview, PDO::PARAM_STR);
-        $query->bindParam(':item_img', $item_img, PDO::PARAM_STR);
-        $query->bindParam(':availability', $availability, PDO::PARAM_STR);
-
-        $query->execute();
-        $lastInsertId = $dbh->lastInsertId();
-        if ($lastInsertId) {
-            $msg = "Facilities posted successfully";
+        if (!in_array($extension, ['jpg', 'png', 'jpeg'])) {
+            echo "Your file extension must be .jpg, .png, or .jpeg";
+        } elseif ($_FILES['item_img']['size'] > 100000000) {
+            echo "File too large!";
         } else {
-            $error = "Something went wrong. Please try again";
+            if (move_uploaded_file($temp_img, $destination1)) {
+                // Move to the second folder
+                if (file_exists($destination1)) {
+                    if (copy($destination1, $destination2)) {
+                        $sql = "INSERT INTO item(item_name, item_overview, item_img, availability) VALUES (:item_name, :item_overview, :item_img, :availability)";
+                        $query = $dbh->prepare($sql);
+                        $query->bindParam(':item_name', $item_name, PDO::PARAM_STR);
+                        $query->bindParam(':item_overview', $item_overview, PDO::PARAM_STR);
+                        $query->bindParam(':item_img', $item_img, PDO::PARAM_STR);
+                        $query->bindParam(':availability', $availability, PDO::PARAM_STR);
+                        
+                        if ($query->execute()) {
+                            $msg = "Facilities posted successfully";
+                        } else {
+                            $error = "Something went wrong. Please try again";
+                        }
+                    } else {
+                        echo "Failed to copy file to the second folder.";
+                    }
+                } else {
+                    echo "Source file does not exist in the first folder.";
+                }
+            } else {
+                echo "Failed to upload file.";
+            }
         }
-
     }
-
+   
+    
 
     ?>
     <!doctype html>
