2a45275cb234ede11dae7671f4b59e9212eb4903
yanwong127 <rezeyan127@gmail.com>
2023-11-21 09:15:29 +0800

    phpmailer done

---
+++ admin/includes/config2.php
@@ -0,0 +1,14 @@
+<?php
+$host = "localhost";
+$dbname = "facilities_book";
+$username = "root";
+$password = "";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
