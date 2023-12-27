29b66bea402df550e1d152ced8d21885c13b8438
Kelvin Chan <chankelvin53@gmai.com>
2023-12-27 09:15:38 +0800

    merge

--- user/db2.php
+++ user/db2.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $dbname = "facilities_book";
 $username = "root";
-$password = "ak6403012";
+$password = "";
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
