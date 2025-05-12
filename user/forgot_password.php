f7f931cde0cabc9d4c96064fafc2090b5369222e
Kelvin Chan <chankelvin53@gmai.com>
2025-05-12 17:18:04 +0800

    merge

--- user/forgot_password.php
+++ user/forgot_password.php
@@ -72,7 +72,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Forgot Password</title>
     <link rel="stylesheet" href="loginsignup.css">
-
 </head>
 <body>
     <div>
